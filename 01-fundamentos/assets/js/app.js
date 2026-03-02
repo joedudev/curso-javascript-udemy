bcb285a09fbd3e5fbc79e8c66a1a306106468f83
Eduardo Reyes <joeduardoreyes@outlook.com>
2026-03-02 15:49:06 -0600

    feat(basics): :rocket: add the assets folder and refactor some code in lesson 14

---
+++ 01-fundamentos/assets/js/app.js
@@ -0,0 +1,20 @@
+// console.log(console.log("Hola Mundo!"));
+
+let a = 10,
+	b = 20,
+	c = "Hola ",
+	d = "Spiderman",
+	x = a + b;
+
+const saludo = c + d;
+
+// console.log("%c Mis Variables", "color:blue; font-weight: bold;");
+// console.log({ a });
+// console.log({ b });
+// console.log({ c });
+
+c = "Hola de nuevo";
+
+// console.table({ a, b, c, d, x });
+
+var miNombre = "Eduardo";
