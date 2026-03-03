c55d62750e3e71ee4a3ad9294e139b52bc71b83c
Eduardo Reyes <joeduardoreyes@outlook.com>
2026-03-03 10:34:10 -0600

    feat(fundamentals): :rocket: add arreglos.js to show array example in lesson 23

---
+++ 01-fundamentos/assets/js/arreglos.js
@@ -0,0 +1,24 @@
+// const arr = [];
+// console.log(arr);
+// console.log(arr.length);
+
+let videojuegos = ["Mario 3", "Megaman", "Chrono Trigger"];
+// console.log({ videojuegos });
+console.log(videojuegos[0]);
+// console.log(videojuegos[1]);
+// console.log(videojuegos[2]);
+
+let arregloCosas = [
+	true,
+	123,
+	"Eduardo",
+	1 + 2,
+	function () {},
+	() => {},
+	{ a: 1 },
+	["X", "Megaman", "Zero", "Dr. Light", ["Dr. Willy", "Woodman"]],
+];
+// console.log({ arregloCosas });
+// console.log(arregloCosas[0]);
+// console.log(arregloCosas[2]);
+console.log(arregloCosas[7][4][1]);
