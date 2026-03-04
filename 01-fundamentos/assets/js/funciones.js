96bf3b85ded683201c814553e366504bd3a392e6
Eduardo Reyes <joeduardoreyes@outlook.com>
2026-03-04 09:02:37 -0600

    feat(fundamentals): :rocket: add more funcitons and examples in lesson 29

--- 01-fundamentos/assets/js/funciones.js
+++ 01-fundamentos/assets/js/funciones.js
@@ -1,6 +1,8 @@
 function saludar(nombre) {
-	console.log([arguments]);
-	console.log("Hola " + nombre);
+	// console.log([arguments]);
+	// console.log("Hola " + nombre);
+	return [1, 2, 3];
+	console.log("soy un código muerto");
 }
 
 const saludar2 = function (nombre) {
@@ -14,7 +16,28 @@ const saludarFlecha2 = (nombre) => {
 	console.log("Hola flecha " + nombre);
 };
 
-saludar("Eduardo", 27, true, "Guatemala");
-saludar2("Eduardo");
-saludarFlecha();
-saludarFlecha2("Eduardo");
+const retornoDeSaludar = saludar("Eduardo", 27, true, "Guatemala");
+console.log(
+	"El retorno de saludar es: " + retornoDeSaludar[0] + retornoDeSaludar[1],
+);
+// saludar2("Eduardo");
+// saludarFlecha();
+// saludarFlecha2("Eduardo");
+
+function sumar(a, b) {
+	return a + b;
+}
+
+console.log(sumar(2, 3));
+
+const sumar2 = (a, b) => a + b;
+console.log(sumar2(5, 10));
+
+function getAleatorio() {
+	return Math.random();
+}
+
+console.log(getAleatorio());
+
+const getAleatorio2 = () => Math.random();
+console.log(getAleatorio2());
