4f1992fc1dcc7efc5d1be92f82c1d91a6ffee591
Eduardo Reyes <joeduardoreyes@outlook.com>
2026-03-05 09:34:51 -0600

    feat(logic-flow): :rocket: completed else-if excersice

--- 01-fundamentos/assets/js/if-else.js
+++ 01-fundamentos/assets/js/if-else.js
@@ -13,20 +13,14 @@ let dia = hoy.getDay();
 
 console.log({ dia });
 
-if (dia === 0) {
-	console.log("Domingo");
-} else if (dia === 1) {
-	console.log("Lunes");
-} else if (dia === 2) {
-	console.log("Martes");
-} else if (dia === 3) {
-	console.log("Miércoles");
-} else if (dia === 4) {
-	console.log("Jueves");
-} else if (dia === 5) {
-	console.log("Viernes");
-} else if (dia === 6) {
-	console.log("Sábado");
-} else {
-	console.log("No es un día válido");
-}
+let diasDeSemana = [
+	"Domingo",
+	"Lunes",
+	"Martes",
+	"Miércoles",
+	"Jueves",
+	"Viernes",
+	"Sábado",
+];
+
+console.log(`Hoy es ${diasDeSemana[dia]}`);
