dcf82b8ae34be01625b461dc2340ed08940741a1
Eduardo Reyes <joeduardoreyes@outlook.com>
2026-03-05 10:38:08 -0600

    feat(logic-flow): :sparkles: Pro-tip: add examples for assignations with conditioning

--- 01-fundamentos/assets/js/logica-booleana.js
+++ 01-fundamentos/assets/js/logica-booleana.js
@@ -43,3 +43,20 @@ console.log(true || false);
 console.log("4 condiciones", true || true || true || false); // true
 
 console.warn("Asignaciones");
+
+const soyUndefined = undefined;
+const soyNull = null;
+const soyFalso = false;
+
+const a1 = false && "Hola Mundo" && 150;
+const a2 = "Hola" && "Mundo" && soyFalso && true;
+const a3 = soyFalso || "Ya no soy falso";
+const a4 =
+	soyFalso || soyUndefined || soyNull || "Ya no soy falso de nuevo" || true;
+const a5 =
+	soyFalso ||
+	soyUndefined ||
+	regresaFalse() ||
+	"Ya no soy falso de nuevo" ||
+	true;
+console.log({ a1, a2, a3, a4, a5 });
