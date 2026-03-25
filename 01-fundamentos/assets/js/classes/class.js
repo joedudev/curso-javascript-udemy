333d3e47d26f8dcc824dc46e395a323a21398215
Eduardo Reyes <joeduardoreyes@outlook.com>
2026-03-25 08:20:26 -0600

    feat(classes-private): :rocket: add getters and setters exampoles to the class in lesson 75

--- 01-fundamentos/assets/js/classes/class.js
+++ 01-fundamentos/assets/js/classes/class.js
@@ -1,45 +1,26 @@
-/**
- * CLASE: Es el "molde" o "plano" de construcción.
- * No es el objeto en sí, sino las instrucciones de cómo debe ser.
- */
 class Persona {
-	// Propiedades de clase (opcionales declararlas aquí, pero es buena práctica)
 	nombre = "";
 	codigo = "";
 	frase = "";
-
-	/**
-	 * CONSTRUCTOR: Es el primer método que se ejecuta al hacer "new Persona()".
-	 * Sirve para inicializar los valores del objeto.
-	 */
+	comida = "";
 	constructor(
 		nombre = "Sin Nombre",
 		codigo = "Sin codigo",
 		frase = "Sin Frase",
 	) {
-		console.log(
-			"%c--- Constructor Ejecutado ---",
-			"color: #f39c12; font-weight: bold;",
-		);
-
-		// Validación lógica
 		if (!nombre) throw Error("Necesitamos el nombre");
-
-		/**
-		 * 'this': Hace referencia a la INSTANCIA específica que estamos creando.
-		 * Es como decir: "A ESTE objeto que estoy fabricando, ponle este nombre".
-		 */
 		this.nombre = nombre;
 		this.codigo = codigo;
 		this.frase = frase;
-
-		console.log(
-			`%c✔ Instancia de ${this.codigo} creada correctamente`,
-			"color: #2ecc71;",
-		);
 	}
 
-	// MÉTODO: Una función que define el comportamiento del objeto
+	set setComidaFavorita(comida) {
+		this.comida = comida.toUpperCase();
+	}
+	get getComidaFavorita() {
+		return `La comida favorita de ${this.nombre} es ${this.comida}`;
+	}
+
 	quienSoy() {
 		console.log(
 			`%c[quienSoy]: Soy ${this.codigo} y mi identidad es ${this.nombre}`,
@@ -48,7 +29,6 @@ class Persona {
 	}
 
 	miFrase() {
-		// Podemos llamar a otros métodos de la misma clase usando 'this'
 		this.quienSoy();
 		console.log(
 			`%c[miFrase]: ${this.codigo} dice: ${this.frase}`,
@@ -57,11 +37,7 @@ class Persona {
 	}
 }
 
-// INSTANCIACIÓN: Crear objetos reales a partir del molde (Clase)
-console.log(
-	"%c\nSOLICITANDO NUEVOS HÉROES...",
-	"background: #222; color: #bada55; padding: 5px;",
-);
+// Heroes
 
 const spiderman = new Persona(
 	"Peter Parker",
@@ -71,13 +47,9 @@ const spiderman = new Persona(
 
 const ironman = new Persona("Tony Stark", "IronMan", "Yo soy Ironman");
 
-// Imprimimos los objetos completos para ver su estructura
-console.log(
-	"%c\nESTADO DE LOS OBJETOS EN MEMORIA:",
-	"font-weight: bold; text-decoration: underline;",
-);
-console.table({ spiderman, ironman }); // console.table es genial para comparar objetos
+spiderman.setComidaFavorita = "El pie de cereza de la tia May";
 
-// Ejecución de métodos
-spiderman.miFrase();
-ironman.miFrase();
+// spiderman.comida = "Duende verde";
+
+console.log(spiderman);
+console.log(spiderman.getComidaFavorita);
