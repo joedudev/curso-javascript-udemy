8b29b359291f2edea3bd86a7150b135a3e048a45
Eduardo Reyes <joeduardoreyes@outlook.com>
2026-03-31 10:15:03 -0600

    refactor(vite-env): :recycle: just refactor the index barrel file of blackjack components

--- 04-blackjack-vite/src/blackjack/usecases/index.js
+++ 04-blackjack-vite/src/blackjack/usecases/index.js
@@ -1,6 +1,6 @@
 export { crearCartaHTML } from "./crear-carta";
 export { crearDeck } from "./crear-deck";
+export { gestionarBotones } from "./gestionar-botones";
 export { pedirCarta } from "./pedir-carta";
 export { turnoComputadora } from "./turno-computadora";
 export { valorCarta } from "./valor-carta";
-export { gestionarBotones } from "./gestionar-botones";
